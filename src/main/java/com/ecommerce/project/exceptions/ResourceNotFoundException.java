db958ff406d266401b573dd1610fd27c374b92ec
pawan <pa211nagar@gmail.com>
2025-07-06 22:30:04 +0530

    ok

--- src/main/java/com/ecommerce/project/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/ecommerce/project/exceptions/ResourceNotFoundException.java
@@ -10,7 +10,7 @@ public class ResourceNotFoundException extends RuntimeException {
     }
 
     public ResourceNotFoundException(String resourceName, String field, String fieldName) {
-        super(String.format("%s not found with %s: %s", resourceName, field, fieldName));
+        super(String.format("%s not found with ok %s: %s", resourceName, field, fieldName));
         this.resourceName = resourceName;
         this.field = field;
         this.fieldName = fieldName;
