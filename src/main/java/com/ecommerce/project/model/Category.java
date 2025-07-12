e0db3bf529a7b7d2a5917f8af0bcdf01a43fc1d3
pawan <pa211nagar@gmail.com>
2025-07-12 21:09:26 +0530

    ok

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -20,7 +20,7 @@ public class Category {
     private Long categoryId;
 
     @NotBlank
-    @Size(min = 5, message = "Category name must contain atleast 5 characters, ok" +
+    @Size(min = 5, message = "Category name must contain atleast 5 characters" +
             " ")
     private String categoryName;
 }
